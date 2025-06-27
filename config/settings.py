00ca75eb78ae83acc42db01628d1f3dbd957ea06
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-06-27 22:38:11 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,8 +4,8 @@ class Config:
     # API Keys
     FOOTBALL_API_KEY = os.getenv("FOOTBALL_API_KEY", "f51d01426524472192e4192e1aa0c842")
     WEATHER_API_KEY = os.getenv("WEATHER_API_KEY", "07522d8003069c55cbc36f67ad21abe5")
-    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "your_telegram_bot_token")
-    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "your_telegram_chat_id")
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8093228828:AAHdg-LOkRx4EpFUpJ6tCcUmxY6YmfIkyxc")
+    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "-1002711171991")
     
     # System Parameters
     MAX_MATCHES = 16
